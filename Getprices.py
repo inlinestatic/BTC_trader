d42d4506f360c7b9bccc7b7f462931dd78b6af0f
happybool <110206015+happybool@users.noreply.github.com>
2023-05-11 09:15:19 -0700

    unifying structures

--- Getprices.py
+++ Getprices.py
@@ -4,8 +4,7 @@ import time
 import threading
 from BinanceApi import BinanceClient
 from KucoinApi import KucoinClient
-
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
 def fetch_json(url):
     for attempt in range(3):
@@ -31,116 +30,197 @@ def fetch_json(url):
                 time.sleep(1)
             else:
                 raise e
-
-def fetch_data_binance():
-    try:
-        #data = fetch_json('https://api.binance.us/api/v3/ticker/price')        
-        book_data = fetch_json('https://api.binance.us/api/v3/ticker/bookTicker')
-        #book_dict = {info['symbol'] for info in book_data if float(info["bidQty"])!=0}
-
-        # Get the current prices for all USD pairs
-        usd_pairs_prices = {}
-        for pair in book_data:
-            if float(pair["bidQty"])!=0:
-                usd_pairs_prices.update({pair['symbol'].upper(): (pair['symbol'],float(pair['bidPrice']))})
-        return usd_pairs_prices
-
-    except requests.HTTPError as http_err:
-        print(f'Binance HTTP error occurred: {http_err}')  # The request returned an unsuccessful status code
-    except Exception as err:
-        print(f'Binance Other error occurred: {err}')  # Any other exception
-        
-def fetch_data_bybit():
-    try:
-        data = fetch_json('https://api.bybit.com/v2/public/tickers')
-
-        usd_pairs_prices = {}
-        for pair in data["result"]:
-            pair_name = pair['symbol']
-            usd_pairs_prices.update({pair_name.upper(): (pair_name, float(pair['last_price']))})
-        return usd_pairs_prices
-
-    except requests.HTTPError as http_err:
-        print(f'Bybit HTTP error occurred: {http_err}')  # The request returned an unsuccessful status code
-    except Exception as err:
-        print(f'Bybit Other error occurred: {err}')  # Any other exception
-
-def fetch_data_kraken():
-    try:
-        data = fetch_json('https://api.kraken.com/0/public/Ticker')
-        pairs_prices = {}
-        for pair, info in data["result"].items():
-            last_trade_price = float(info['c'][0])  # 'c' is the field for last trade closed price in Kraken API
-            pairs_prices.update({pair.upper(): (pair, last_trade_price)})
-        return pairs_prices
-
-    except requests.HTTPError as http_err:
-        print(f'Kraken HTTP error occurred: {http_err}')  # The request returned an unsuccessful status code
-    except Exception as err:
-        print(f'Kraken Other error occurred: {err}')  # Any other exception
-
-def fetch_data_kucoin():
-    try:
-        data = fetch_json('https://api.kucoin.com/api/v1/market/allTickers')
-
-        pairs_prices = {}
-        for info in data["data"]["ticker"]:
-            if info['last'] is not None:
-                symbol = info['symbol'].replace('-','')            
-                last_price = float(info['last'])  # 'last' is the field for last price in KuCoin API
-                pairs_prices.update({symbol.upper(): (info['symbol'], last_price)})
-        return pairs_prices
-
-    except requests.HTTPError as http_err:
-        print(f'KuCoin HTTP error occurred: {http_err}')  # The request returned an unsuccessful status code
-    except Exception as err:
-        print(f'KuCoin Other error occurred: {err}')  # Any other exception
-
-if __name__ == "__main__":
-    exchange_names = ['Binance', 'Bybit', 'Kraken', 'KuCoin']
-    exchanges = [fetch_data_binance, fetch_data_bybit, fetch_data_kraken, fetch_data_kucoin]
-
-    #exchange_names = ['Binance', 'KuCoin']
-    #exchanges = [fetch_data_binance, fetch_data_kucoin]
+                
+def clean_symbol(symbol):
+    return symbol.replace('-', '').replace('_', '').replace(':', '')
     
-    exchane_id = 0
-    while True:
-        with ThreadPoolExecutor(max_workers=len(exchanges)) as executor:
-            results = list(executor.map(lambda f: f(), exchanges))
+def get_binance_prices():
+    url = 'https://api.binance.com/api/v3/ticker/bookTicker'
+    response = requests.get(url)
+    data = response.json()
+    prices = {}
+    for pair_data in data:
+        symbol = pair_data['symbol']
+        ask_price = pair_data['askPrice']
+        bid_price = pair_data['bidPrice']
+        pair = clean_symbol(symbol)
+        prices[pair] = {
+            'pair': pair,
+            'sym': symbol,
+            'askPrice': ask_price,
+            'bidPrice': bid_price
+        }
+    return prices
         
-        exchanges_result = {i: result for i, result in enumerate(results)}
-        price_symbol_map = {}
-        for i, exchange_pairs in exchanges_result.items():
-            for exchange_name, (symbol, price) in exchange_pairs.items():
-                if exchange_name not in price_symbol_map:
-                    price_symbol_map[exchange_name] = {'prices': [0.0]*len(exchanges), 'symbols': ['']*len(exchanges)}
-                price_symbol_map[exchange_name]['prices'][i] = price
-                price_symbol_map[exchange_name]['symbols'][i] = symbol
-        
-        to_add = list()
-        for pair, data in price_symbol_map.items():
-            prices = data['prices']
-            symbols = data['symbols']
-            min_val = min((price for price in prices if price != 0), default=float('inf'))
-            max_val = max(prices)
-        
-            if min_val > 0 and max_val > 0 and min_val != max_val:
-                percentile_diff = ((max_val - min_val) / min_val) * 100
-                if 2 < percentile_diff < 20:
-                    min_index = prices.index(min_val)
-                    max_index = prices.index(max_val)
-                    trade = {
-                        'buy_from': exchange_names[min_index],
-                        'buy_symbol': symbols[min_index],
-                        'sell_to': exchange_names[max_index],
-                        'sell_symbol': symbols[max_index],
-                        'buy_price': min_val,
-                        'sell_price': max_val,
-                        'diff': percentile_diff
-                    }
-                    to_add.append(trade)
-                    print(trade)
-        to_add.sort(key=lambda x: x['diff'], reverse=True)
+def get_bybit_prices():
+    url = 'https://api.bybit.com/v2/public/tickers'
+    response = requests.get(url)
+    data = response.json()['result']
+    prices = {}
+    for pair_data in data:
+        symbol = pair_data['symbol']
+        ask_price = pair_data['ask_price']
+        bid_price = pair_data['bid_price']
+        pair = clean_symbol(symbol)
+        prices[pair] = {
+            'pair': pair,
+            'sym': symbol,
+            'askPrice': ask_price,
+            'bidPrice': bid_price
+        }
+    return prices
+
+def get_kraken_prices():
+    url = 'https://api.kraken.com/0/public/Ticker'
+    response = requests.get(url)
+    data = response.json()['result']
+    prices = {}
+    for pair, pair_data in data.items():
+        symbol = pair
+        ask_price = pair_data['a'][0]
+        bid_price = pair_data['b'][0]
+        pair = clean_symbol(symbol)
+        prices[pair] = {
+            'pair': pair,
+            'sym': symbol,
+            'askPrice': ask_price,
+            'bidPrice': bid_price
+        }
+    return prices
+
+def get_kucoin_prices():
+    url = 'https://api.kucoin.com/api/v1/market/allTickers'
+    response = requests.get(url)
+    data = response.json()['data']['ticker']
+    prices = {}
+    for pair_data in data:
+        symbol = pair_data['symbol']
+        ask_price = pair_data['sell']
+        bid_price = pair_data['buy']
+        pair = clean_symbol(symbol)
+        prices[pair] = {
+            'pair': pair,
+            'sym': symbol,
+            'askPrice': ask_price,
+            'bidPrice': bid_price
+        }
+    return prices
+
+
+exchange_names = ['Binan', 'Bybit', 'Krake', 'KuCoi']
+
+def find_arbitrage_opportunities_parallel(prices):
+    matches = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                if i != j:
+                    future = executor.submit(check_prices, prices[i], prices[j])
+                    futures.append(future)
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result is not None:
+                matches.append(result)
+    return matches
+
+def check_prices(prices1, prices2):
+    matches = []
+    for key1, price1 in prices1[1].items():
+        for key2, price2 in prices2[1].items():
+            if price1['pair'] == price2['pair']:
+            
+                ask_price2 = float(price2['askPrice'])
+                bid_price1 = float(price1['bidPrice'])
+                
+                ask_price1 = float(price1['askPrice'])
+                bid_price2 = float(price2['bidPrice'])
+                
+                if bid_price1 > ask_price2:
+                    if ask_price2 > 0.0:
+                        diff = (bid_price1 - ask_price2) / ask_price2 * 100
+                        if diff > 0.8:
+                            match = {
+                                'buy_from': exchange_names[prices2[0]],
+                                'sell_to': exchange_names[prices1[0]],
+                                'bid_from': bid_price2,
+                                'ask_from': ask_price2,
+                                'bid_to': bid_price1,
+                                'ask_to': ask_price1,
+                                'symbol_buy': price2['sym'],
+                                'symbol_sell': price1['sym'],
+                                'sym': price1['pair'],
+                                'diff': diff
+                            }
+                            matches.append(match)
+                elif bid_price2 > ask_price1:
+                    if ask_price1 > 0.0:
+                        diff = (bid_price2 - ask_price1) / ask_price1 * 100
+                        if diff > 0.8:
+                            match = {
+                                'buy_from': exchange_names[prices1[0]],
+                                'sell_to': exchange_names[prices2[0]],
+                                'bid_from': bid_price1,
+                                'ask_from': ask_price1,
+                                'bid_to': bid_price2,
+                                'ask_to': ask_price2,
+                                'symbol_buy': price1['sym'],
+                                'symbol_sell': price2['sym'],
+                                'sym': price1['pair'],
+                                'diff': diff
+                            }
+                            matches.append(match)
+    print(matches)
+    return matches
+
+
+if __name__ == '__main__':
+    exchanges = [get_bybit_prices, get_bybit_prices, get_kraken_prices, get_kucoin_prices]
+    prices = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(len(exchanges)):
+            future = executor.submit(exchanges[i])
+            futures.append(future)
+        i=0
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            prices.append((i,result))
+            i+=1
+            
+    matches = find_arbitrage_opportunities_parallel(prices)
+        #price_symbol_map = {}
+        #for i, exchange_pairs in exchanges_result.items():
+        #    for exchange_name, (symbol, price) in exchange_pairs.items():
+        #        if exchange_name not in price_symbol_map:
+        #            price_symbol_map[exchange_name] = {'prices': [0.0]*len(exchanges), 'symbols': ['']*len(exchanges)}
+        #        price_symbol_map[exchange_name]['prices'][i] = price
+        #        price_symbol_map[exchange_name]['symbols'][i] = symbol
+        #
+        #to_add = list()
+        #for pair, data in price_symbol_map.items():
+        #    prices = data['prices']
+        #    symbols = data['symbols']
+        #    min_val = min((price for price in prices if price != 0), default=float('inf'))
+        #    max_val = max(prices)
+        #
+        #    if min_val > 0 and max_val > 0 and min_val != max_val:
+        #        percentile_diff = ((max_val - min_val) / min_val) * 100
+        #        if 2 < percentile_diff < 20:
+        #            min_index = prices.index(min_val)
+        #            max_index = prices.index(max_val)
+        #            trade = {
+        #                'buy_from': exchange_names[min_index],
+        #                'buy_symbol': symbols[min_index],
+        #                'sell_to': exchange_names[max_index],
+        #                'sell_symbol': symbols[max_index],
+        #                'buy_price': min_val,
+        #                'sell_price': max_val,
+        #                'diff': percentile_diff
+        #            }
+        #            to_add.append(trade)
+        #            print(trade)
+        #to_add.sort(key=lambda x: x['diff'], reverse=True)
         
         #current_trade = None
         #for trade in to_add:
